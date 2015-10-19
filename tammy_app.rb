aa1f0544346c7e815aa5572105f3c3ce93e267c3
smurfik <smurfik@mlpinit.com>
2015-10-18 20:47:26 -0700

    add username file, update post, refresh

--- tammy_app.rb
+++ tammy_app.rb
@@ -1,54 +1,58 @@
 require_relative 'bubble'
 time_format = "%m-%e-%y %H:%M"
-puts "What is your username? type exit or done to see the feed"
+path_for_username = "/Users/tamarapop/code_builders/projects/soapbox/username"
+
+the_name = ""
+
+File.open(path_for_username) do |f|
+  the_name = f.read
+  puts "Welcome! If you're #{the_name.blue}, hit Y, otherwise type your username."
+end
 
 username = gets.chomp.downcase
 
 while true
   break if username == "exit" || username == "done"
-  puts "What would you like to post? type exit or done to see the feed"
-  body = gets.chomp
-  break if body == "exit" || body == "done"
-
-  answers = {
-    username:    username,
-    body:        body,
-    created_at:  Time.now.strftime(time_format)
-  }
-
-  b = Bubble.new(answers)
-  b.write_file
-
-end
-
-# all_files = Dir.glob("/Users/tamarapop/Dropbox/SoapBox/*")
-all_files = Dir.glob("/Users/tamarapop/code_builders/practice/trees/*")
-
-sorted_files = all_files.sort_by { |x| File.birthtime(x) }.reverse
-
-
-sorted_files.first(10).each do |element|
-  File.open(element) do |the_file|
-    answers = {
-      username: the_file.read,
-      body: File.basename(element),
-      created_at: the_file.birthtime.strftime(time_format)
-    }
-    nb = Bubble.new(answers)
-    puts nb.formatted_string
-  end
-end
-
-while true
-  puts "Would you like to post, refresh, or exit?"
-  answer = gets.chomp.downcase
-  break if answer == "exit"
-
-  if answer == "post"
-
-  elsif answer == "refresh"
-
+  if username == "y"
+    username = the_name
   else
-    puts "Could you repeat that?"
+    File.open(path_for_username, "w") do |f|
+      f << username
+    end
+  end
+
+  puts "Would you like to post, refresh, or exit?"
+  body = gets.chomp.downcase
+
+  break if body == "exit"
+  if body == "post"
+    puts "Write your message:"
+    body = gets.chomp.downcase
+    answers = {
+      username:    username,
+      body:        body,
+      created_at:  Time.now.strftime(time_format)
+    }
+
+    b = Bubble.new(answers)
+    b.write_file
+    puts b.formatted_string
+  else
+    # all_files = Dir.glob("/Users/tamarapop/Dropbox/SoapBox/*")
+    all_files = Dir.glob("/Users/tamarapop/code_builders/practice/trees/*")
+
+    sorted_files = all_files.sort_by { |x| File.birthtime(x) }.reverse
+
+    sorted_files.first(10).each do |element|
+      File.open(element) do |the_file|
+        answers = {
+          username: the_file.read,
+          body: File.basename(element),
+          created_at: the_file.birthtime.strftime(time_format)
+        }
+        nb = Bubble.new(answers)
+        puts nb.formatted_string
+      end
+    end
   end
 end
